bbe8151633514285ee083283d422bf8c2eab8b38
Hooshyar <hooseyr@gmail.com>
2022-02-15 13:27:34 +0300

    example added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # flutter_send_FCM_Api
 Flutter code to send push notifications using Firebase Cloud Messaging easy way
+
+
+## Packages 
+add the http package to your project
+```yaml
+dependencies:
+  flutter:
+    sdk: flutter
+  http: 
+```
+## FCM Server Key
+get your FCM server key form the firebase project setting FCM section
+
+replace the serverKey string in fcm_sender_service.dart 
+
+```dart
+  final String serverKey = "replace your api with this string";
+```
---
+++ lib/services/fcm_sender_service.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class NotificationsService {
+  //TODO: replace this with your own server serverKey, could be found from setting FCM section
+  final String serverKey = "replace your api with this string";
+
+  // final String senderID = "214993471933";
+
+  String _constructFCMPayload({
+    required String token,
+    required String title,
+    required String body,
+  }) {
+    return jsonEncode({
+      'to': token,
+      'data': {
+        'via': 'FlutterFire Cloud Messaging!!!',
+        'url': 'https://fcm.googleapis.com/fcm/send',
+      },
+      "collapse_key": "type_a",
+      'notification': {'title': title, 'body': body, 'sound': "default"},
+      "priority": "high",
+      "apns": {
+        "headers": {"apns-priority": "10"},
+        "payload": {
+          "aps": {
+            "headers": {
+              'apns-priority': '10',
+            },
+            "alert": {"body": "$title", "title": "$body"},
+            "badge": 2,
+            "sound": "default"
+          }
+        }
+      },
+      "android": {
+        "priority": 'high',
+        "notification": {
+          "sound": 'default',
+        }
+      },
+    });
+  }
+
+  Future<void> sendPushMessage({
+    required List<String> tokens,
+    required String title,
+    required String body,
+  }) async {
+    for (int i = 0; i < tokens.length; i++) {
+      try {
+        await http.post(
+          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Authorization': 'key=$serverKey',
+          },
+          body:
+              _constructFCMPayload(token: tokens[i], title: title, body: body),
+        );
+        print('FCM request for device sent!');
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+}
